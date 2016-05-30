541dac9b9c8cc6a212a66236631527c95cb80b47
Peter(Yuan) Wang <yuan.wang@yuanwang-ltm3.internal.salesforce.com>
2016-05-29 20:14:03 -0700

    new files

--- views/index.jsx
+++ views/index.jsx
@@ -3,21 +3,25 @@ import React from 'react';
 export default class TodoBox extends React.Component {
     render() {
         return (
-            <div className="todoBox">
-                <h1>Todos</h1>
-                <TodoList />
-                <TodoForm />
+            <div className="todoList">
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
 
-class TodoList extends React.Component {
+class Todo extends React.Component {
   render() {
   	return (
-  		<div className="todoList">
-      I am a TodoList.
-    </div>
+  		<tr>
+  			<td style="border:1px solid black;"></td>
+            <td style="border:1px solid black;">13:00</td>
+  		</tr>
   		);
   }
 }
